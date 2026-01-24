ea3426439de414532de5922d2cad0aeaa931c10b
Pedro Marques <pedromarques391@gmail.com>
2026-01-24 20:23:20 -0300

    path: change dir.

---
+++ src/components/shared/ui/MenuItens.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+
+
+interface MenuItemProps {
+  label: string
+  link: string
+}
+const { label, link } = defineProps<MenuItemProps>()
+
+const route = useRoute();
+
+const isActivePath = computed(() => route.path === link);
+
+</script>
+
+<template>
+  <li class="h-full w-auto">
+    <RouterLink :to="link"
+      class="group relative flex h-full items-center px-4 transition-colors p-2 text-base  lg:text-lg"
+      active-class="text-accent font-semibold">
+      {{ label }}
+      <span class="absolute bottom-0 left-0 h-0.75 bg-accent transition-all duration-300 w-0 group-hover:w-full"
+        :class="{ 'w-full': isActivePath }" />
+    </RouterLink>
+  </li>
+</template>
