4aaa8567a3782864f4cdb543414ca190055dfd77
Pedro Marques <pedromarques391@gmail.com>
2026-01-25 00:34:14 -0300

    feat: import CategorySection and ProductSection into HomePage.

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,24 @@
+<script setup lang="ts">
+import CategorySection from '@/components/home/CategorySection.vue';
+import Hero from '@/components/home/Hero.vue';
+import ProductsSection from '@/components/home/ProductsSection.vue';
+import { useProducts } from '@/composables/useProducts';
+import { computed } from 'vue';
+
+const { data: products, isLoading } = useProducts();
+
+const featuredProducts = computed(() => {
+  if (!products.value) return [];
+  return [...products.value]
+    .sort((a, b) => b.rating.rate - a.rating.rate)
+    .slice(0, 6);
+});
+</script>
+
 <template>
-  <h1>Hello from Home</h1>
+  <div class="flex flex-col min-h-screen">
+    <Hero />
+    <CategorySection />
+    <ProductsSection :products="featuredProducts" :is-loading="isLoading" />
+  </div>
 </template>
