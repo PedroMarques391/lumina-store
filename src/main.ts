fb9444548d4fd778a12f2495d528dd6c81ae71b7
Pedro Marques <pedromarques391@gmail.com>
2026-01-24 22:00:18 -0300

    package: add vue-query in project.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,15 @@
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 import './App.css'
 
 import App from './App.vue'
 import router from './router'
+import { VueQueryPlugin } from '@tanstack/vue-query'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueQueryPlugin)
 
 app.mount('#app')
