2a0bb2166b9aeaf434ac11484e2da8c451d4a650
Pedro Marques <pedromarques391@gmail.com>
2026-01-25 00:32:53 -0300

    feat: create component to show categories.

---
+++ src/components/home/CategorySection.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import SectionTitle from '@/components/shared/SectionTitle.vue';
+
+const categories = [
+  {
+    name: 'Jewelery',
+    value: 'jewelery',
+    image: '/images/Jewelery.jpeg'
+  },
+  {
+    name: "Men's clothing",
+    value: "mens-clothing",
+    image: '/images/Mens-Clothing.jpeg'
+  },
+  {
+    name: "Women's clothing",
+    value: "womens-clothing",
+    image: '/images/Womens-Clothing.jpeg'
+  },
+]
+</script>
+
+<template>
+  <section class="py-16 bg-white">
+    <div class="container mx-auto px-4">
+      <SectionTitle title="Browse by Category" subtitle="Find exactly what you are looking for." />
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div v-for="category in categories" :key="category.value"
+          class="group relative overflow-hidden rounded-xl shadow-lg cursor-pointer h-64">
+          <img :src="category.image" :alt="category.name"
+            class="absolute inset-0 w-full h-full object-cover transition-transform duration-500 group-hover:scale-110" />
+          <div
+            class="absolute inset-0 bg-black/40 group-hover:bg-black/50 transition-colors duration-300 flex items-center justify-center">
+            <h3
+              class="text-base md:text0lg lg:text-2xl font-bold text-white uppercase tracking-wider border-b-2 border-transparent group-hover:border-white transition-all pb-1">
+              {{ category.name }}
+            </h3>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
