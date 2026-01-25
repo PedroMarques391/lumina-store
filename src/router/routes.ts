069f97e72a13387f26f9d03c35e7cbafa64e1528
Pedro Marques <pedromarques391@gmail.com>
2026-01-25 00:11:23 -0300

    feat: create all routers of the project and add in routes.ts.

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,5 +1,8 @@
 import AppLayout from '@/components/layout/AppLayout.vue'
+import AboutUsPage from '@/pages/AboutUsPage.vue'
+import ContactPage from '@/pages/ContactPage.vue'
 import HomePage from '@/pages/HomePage.vue'
+import ProductPage from '@/pages/ProductPage.vue'
 import type { RouteRecordRaw } from 'vue-router'
 
 export const routes: RouteRecordRaw[] = [
@@ -12,6 +15,42 @@ export const routes: RouteRecordRaw[] = [
         path: '',
         name: 'HomePage',
         component: HomePage,
+      },
+    ],
+  },
+  {
+    path: '/products',
+    name: 'Product',
+    component: AppLayout,
+    children: [
+      {
+        path: '',
+        name: 'ProductPage',
+        component: ProductPage,
+      },
+    ],
+  },
+  {
+    path: '/contacts',
+    name: 'Contact',
+    component: AppLayout,
+    children: [
+      {
+        path: '',
+        name: 'ContactPage',
+        component: ContactPage,
+      },
+    ],
+  },
+  {
+    path: '/about-us',
+    name: 'AboutUs',
+    component: AppLayout,
+    children: [
+      {
+        path: '',
+        name: 'AboutUsPage',
+        component: AboutUsPage,
       },
     ],
   },
