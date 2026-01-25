bbd719ea9f449962081dc1f43f69381239a1497d
Pedro Marques <pedromarques391@gmail.com>
2026-01-25 10:31:33 -0300

    feat: create dynamic router to product category.

---
+++ src/pages/ProductPageByCategory.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Product by category</h1>
+</template>
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,6 +3,7 @@ import AboutUsPage from '@/pages/AboutUsPage.vue'
 import ContactPage from '@/pages/ContactPage.vue'
 import HomePage from '@/pages/HomePage.vue'
 import ProductPage from '@/pages/ProductPage.vue'
+import ProductPageByCategory from '@/pages/ProductPageByCategory.vue'
 import type { RouteRecordRaw } from 'vue-router'
 
 export const routes: RouteRecordRaw[] = [
@@ -27,6 +28,11 @@ export const routes: RouteRecordRaw[] = [
         path: '',
         name: 'ProductPage',
         component: ProductPage,
+      },
+      {
+        path: ':category',
+        name: 'ProductPageCategory',
+        component: ProductPageByCategory,
       },
     ],
   },
