a6899fa817b2119f510dc8acc8d1a0984c081e70
Pedro Marques <pedromarques391@gmail.com>
2026-01-25 23:20:09 -0300

    feat: create skeleton for product card.

---
+++ src/components/shared/ui/ProductCardSkeleton.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+interface SkeletonProps {
+  isLoading: boolean
+}
+
+defineProps<SkeletonProps>()
+
+</script>
+
+<template>
+  <div v-if="isLoading" class="flex flex-col gap-4">
+    <div class="skeleton h-48 w-full"></div>
+    <div class="skeleton h-4 w-28"></div>
+    <div class="skeleton h-4 w-full"></div>
+    <div class="skeleton h-4 w-full"></div>
+    <div class="flex flex-1 justify-between items-end px-2">
+      <span class="skeleton h-4 w-24"></span>
+      <span class="skeleton h-8 w-24"></span>
+    </div>
+  </div>
+</template>
