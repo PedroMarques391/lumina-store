010f7b7d6a7ad71f39051b061444d7280f70fad4
Pedro Marques <pedromarques391@gmail.com>
2026-01-26 16:59:03 -0300

    feat: add fadeUp animation in TimeLineVuew.

--- src/components/about/TimeLine.vue
+++ src/components/about/TimeLine.vue
@@ -7,7 +7,7 @@ import { CheckIcon } from 'lucide-vue-next';
   <section class="pb-20 max-w-4xl mx-auto px-4">
     <h2 class="text-3xl font-bold text-center mb-12">Our Evolution</h2>
 
-    <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+    <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical scroll-timeline">
       <li v-for="(event, index) in timelineEvents" :key="index">
 
         <div class="timeline-middle">
@@ -17,7 +17,7 @@ import { CheckIcon } from 'lucide-vue-next';
         </div>
 
         <div :class="[
-          index % 2 === 0 ? 'timeline-start md:text-end' : 'timeline-end',
+          index % 2 === 0 ? 'timeline-start md:text-end ' : 'timeline-end',
           'mb-10 md:px-4'
         ]">
           <time class="font-mono italic text-sm opacity-60">{{ event.year }}</time>
@@ -27,8 +27,30 @@ import { CheckIcon } from 'lucide-vue-next';
           </p>
         </div>
 
-        <hr class="bg-primary/40 " />
+        <hr class="bg-primary/40" />
       </li>
     </ul>
   </section>
 </template>
+
+<style scoped>
+@keyframes fadeUp {
+  from {
+    opacity: 0;
+    transform: translateY(100px) scale(0.9);
+    filter: blur(10px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateY(0) scale(1);
+    filter: blur(0);
+  }
+}
+
+.scroll-timeline li {
+  animation: fadeUp linear both;
+  animation-timeline: view();
+  animation-range: entry 10% cover 40%;
+}
+</style>
