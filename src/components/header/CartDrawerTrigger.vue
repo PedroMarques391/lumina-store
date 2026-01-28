dc308bf6ab11109a47b26b73c1f092e3b72dfbd5
Pedro Marques <pedromarques391@gmail.com>
2026-01-28 20:01:39 -0300

    feat: create trigger to show ProductCart sidebar.

---
+++ src/components/header/CartDrawerTrigger.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useProductStore } from '@/stores/product';
+import { Handbag } from 'lucide-vue-next';
+import { storeToRefs } from 'pinia';
+
+const store = useProductStore();
+const { itemAmount } = storeToRefs(store);
+</script>
+
+<template>
+  <div class="drawer-content">
+    <label for="cart-drawer" class="indicator cursor-pointer">
+      <span class="indicator-item badge badge-sm badge-secondary" v-if="itemAmount > 0">{{ itemAmount }}</span>
+      <Handbag :size="30" />
+    </label>
+  </div>
+</template>
