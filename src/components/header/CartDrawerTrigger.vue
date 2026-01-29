81c2e79389030ca9c981cf9a4492bf64c8753e14
Pedro Marques <pedromarques391@gmail.com>
2026-01-29 10:02:40 -0300

    fix: removing two drawers on the screen at the same time for mobile version.

--- src/components/header/CartDrawerTrigger.vue
+++ src/components/header/CartDrawerTrigger.vue
@@ -5,11 +5,21 @@ import { storeToRefs } from 'pinia';
 
 const store = useProductStore();
 const { itemAmount } = storeToRefs(store);
+
+const closeMobileDrawer = () => {
+  const drawerCheckbox = document.getElementById('my-drawer-1') as HTMLInputElement;
+
+  if (drawerCheckbox && window.innerWidth < 768) {
+    console.log('fechou')
+    drawerCheckbox.checked = false;
+  }
+}
+
 </script>
 
 <template>
   <div class="drawer-content">
-    <label for="cart-drawer" class="indicator cursor-pointer">
+    <label for="cart-drawer" class="indicator cursor-pointer" @click="closeMobileDrawer">
       <span class="indicator-item badge badge-sm badge-secondary" v-if="itemAmount > 0">{{ itemAmount }}</span>
       <Handbag :size="30" />
     </label>
