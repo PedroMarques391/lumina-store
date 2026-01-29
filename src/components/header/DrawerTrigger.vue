e8433ca392ac833b6b7cf916ef5b8fe37acd5d78
Pedro Marques <pedromarques391@gmail.com>
2026-01-29 10:03:00 -0300

    feat: edit menu svg size.

--- src/components/header/DrawerTrigger.vue
+++ src/components/header/DrawerTrigger.vue
@@ -3,7 +3,7 @@ import { onMounted, onUnmounted } from 'vue';
 
 const closeDrawerOnResize = () => {
   const drawerCheckbox = document.getElementById('my-drawer-1') as HTMLInputElement;
-  
+
   if (drawerCheckbox && window.innerWidth >= 768) {
     drawerCheckbox.checked = false;
   }
@@ -24,9 +24,9 @@ onUnmounted(() => {
     <input id="my-drawer-1" type="checkbox" class="drawer-toggle" />
 
     <div class="drawer-content">
-      <label for="my-drawer-1" class="btn btn-square btn-ghost">
+      <label for="my-drawer-1" class="btn btn-square btn-ghost ">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-          class="inline-block w-6 h-6 stroke-current">
+          class="inline-block w-10 h-10 stroke-current">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16">
           </path>
         </svg>
